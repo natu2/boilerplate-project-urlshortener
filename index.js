d47f232588d136fdabbd252ba5c9e434830eb372
Saili <natu2@wisc.edu>
2024-08-09 18:34:20 -0700

    Getting started

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+
+//TODO: connect to a Mongo DB
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  //TODO
+});
+
+app.get("/api/shorturl/:shortenedUrl", (req, res) => {
+  //TODO
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
